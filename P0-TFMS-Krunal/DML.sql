908907a7999307211e8f207d6639f93297271271
kunal zodape <kunal.zodape@xcarepro.com>
2023-01-08 15:54:07 +0530

    Updated DML file

--- P0-TFMS-Krunal/DML.sql
+++ P0-TFMS-Krunal/DML.sql
@@ -1,2 +1,5 @@
 USE tfms_db_krunal;
 INSERT INTO trainer_details VALUES('1001','GANGADHR PARDE', 'java', 'M.TECH', '10 YRS', '535698753', 'gd@gmail.com');
+INSERT INTO trainer_details VALUES('1002','GANGADHR PARDE', 'java', 'M.TECH', '10 YRS', '535698753', 'gd@gmail.com');
+INSERT INTO trainer_details VALUES('1003','PREETI AGRWAL', 'mainframe', 'M.TECH', '10 YRS', '535698753', 'gd@gmail.com');
+INSERT INTO trainer_details VALUES('1004','AKASH SOLANKE', 'testing', 'M.TECH', '10 YRS', '535698753', 'gd@gmail.com');
